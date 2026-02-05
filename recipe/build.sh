f4459463186b06887fff681b3a4b0f9fd2ee847a
Andrew Howe <ahowe@anaconda.com>
2026-02-05 11:33:56 -0500

    Adapting 0.29.2 for main channel

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,7 +4,7 @@ set -exuo pipefail
 
 export PYPI_RELEASE=1
 export CMAKE_GENERATOR=Ninja
-export DEVELOPER_DIR=/Applications/Xcode.app/Contents/Developer
+export DEVELOPER_DIR=/Library/Developer/CommandLineTools
 export CMAKE_BUILD_PARALLEL_LEVEL=""
 if [[ "${target_platform}" != "osx-arm64" ]]; then
   export BLAS_HOME=$PREFIX
@@ -12,6 +12,7 @@ if [[ "${target_platform}" != "osx-arm64" ]]; then
 elif [[ "$target_platform" == "osx-arm64" && "${CONDA_BUILD_CROSS_COMPILATION:-0}" == "1" ]]; then
   export CMAKE_ARGS="${CMAKE_ARGS} -DCMAKE_OSX_ARCHITECTURES=arm64"
 fi
+export CMAKE_ARGS="${CMAKE_ARGS} -DMLX_BUILD_METAL=OFF"
 if [[ "${target_platform}" == linux-* ]]; then
   export LDFLAGS="-lblas ${LDFLAGS}"
 elif [[ "${target_platform}" == "osx-64" ]]; then
@@ -19,4 +20,4 @@ elif [[ "${target_platform}" == "osx-64" ]]; then
 fi
 export CMAKE_ARGS="${CMAKE_ARGS} -DCMAKE_PREFIX_PATH=${PREFIX};${SP_DIR} -DPython_EXECUTABLE=$PYTHON -DPython_INCLUDE_DIR=${PREFIX}/include/python${PY_VER}"
 
-$PYTHON -m pip install . -vv
+$PYTHON -m pip install . -vv --no-deps --no-build-isolation
