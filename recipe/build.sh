4b8d72bcd7dce13f45f9f6e031875d848aefea0e
Eric Lundby <Eric.Lundby@gmail.com>
2026-04-10 09:35:36 -0600

    enable metal.

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,15 +4,13 @@ set -exuo pipefail
 
 export PYPI_RELEASE=1
 export CMAKE_GENERATOR=Ninja
-export DEVELOPER_DIR=/Library/Developer/CommandLineTools
 export CMAKE_BUILD_PARALLEL_LEVEL=""
 if [[ "${target_platform}" != "osx-arm64" ]]; then
   export BLAS_HOME=$PREFIX
-  export CMAKE_ARGS="${CMAKE_ARGS} -DMLX_BUILD_METAL=OFF"
 elif [[ "$target_platform" == "osx-arm64" && "${CONDA_BUILD_CROSS_COMPILATION:-0}" == "1" ]]; then
   export CMAKE_ARGS="${CMAKE_ARGS} -DCMAKE_OSX_ARCHITECTURES=arm64"
 fi
-export CMAKE_ARGS="${CMAKE_ARGS} -DMLX_BUILD_METAL=OFF"
+
 if [[ "${target_platform}" == linux-* ]]; then
   export LDFLAGS="-lblas ${LDFLAGS}"
 elif [[ "${target_platform}" == "osx-64" ]]; then
